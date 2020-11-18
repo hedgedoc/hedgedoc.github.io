5ad289724206a0d2c6ed997ece5728c386943ccb
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-11-18 20:19:16 +0100

    Change nav dom
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- themes/vex-hugo/static/js/mobile-menu.js
+++ themes/vex-hugo/static/js/mobile-menu.js
@@ -1,3 +1,3 @@
 document.querySelector('button.navbar-toggler').addEventListener('click', () => {
-  document.querySelector('nav div.collapse.navbar-collapse').classList.toggle('show')
+  document.querySelector('nav.collapse.navbar-collapse').classList.toggle('show')
 })
