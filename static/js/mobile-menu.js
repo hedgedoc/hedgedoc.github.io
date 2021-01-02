a748bd80fe9ac15e60f49c46e3cbd43171da3ce0
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2021-01-02 13:54:54 +0100

    Reorganize theme (#52)
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

---
+++ static/js/mobile-menu.js
@@ -0,0 +1,8 @@
+/**
+ SPDX-FileCopyrightText: 2020 The HedgeDoc developers (see AUTHORS file)
+
+ SPDX-License-Identifier: CC-BY-SA-4.0
+ **/
+document.querySelector('button.navbar-toggler').addEventListener('click', () => {
+  document.querySelector('nav.collapse.navbar-collapse').classList.toggle('show')
+})
--- themes/vex-hugo/static/js/mobile-menu.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('button.navbar-toggler').addEventListener('click', () => {
-  document.querySelector('nav.collapse.navbar-collapse').classList.toggle('show')
-})
